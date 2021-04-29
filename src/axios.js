06b3ea249361019a1fa54bfb4400fe78c1cd6b78
Qirol4006 <69517315+Qirol4006@users.noreply.github.com>
2021-04-30 02:47:43 +0500

    asd

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 // axios.defaults.baseURL = 'https://v-back.herokuapp.com'
 // axios.defaults.baseURL = 'http://localhost:6699'
-axios.defaults.baseURL = 'http://videokurs.uz:6699'
+axios.defaults.baseURL = 'https://videokurs.uz:6699'
 
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
