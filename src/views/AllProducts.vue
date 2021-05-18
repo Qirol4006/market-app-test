b784b9f9ad60ddb38eabb4997f65d071d965d5b8
Qirol4006 <69517315+Qirol4006@users.noreply.github.com>
2021-05-19 01:15:18 +0500

    a

--- src/views/AllProducts.vue
+++ src/views/AllProducts.vue
@@ -3,6 +3,16 @@
 
       <div class="content">
 
+        <form class="navbar-form">
+          <div class="input-group no-border col-sm-11">
+            <input @keyup="searchData()" v-model="query" type="text" value="" class="form-control" placeholder="Qidirish...">
+            <button type="submit" class="btn btn-white btn-round btn-just-icon">
+              <i class="material-icons">search</i>
+              <div class="ripple-container"></div>
+            </button>
+          </div>
+        </form>
+
         <div v-if="query" class="container-fluid">
           <div class="row">
             <div class="col-md-12">
--- src/views/SellProduct.vue
+++ src/views/SellProduct.vue
@@ -1,25 +1,19 @@
 <template>
 <div class="card-body justify-content-center form-group">
-  <td class="td-number">
-    1
-    <div class="btn-group btn-group-sm">
-      <button class="btn btn-round btn-info"> <i class="material-icons">remove</i> </button>
-      <button class="btn btn-round btn-info"> <i class="material-icons">add</i> </button>
-    </div>
-  </td>
   <div class="input-group no-border col-sm-12">
 
+
       <div class="input-group no-border col-sm-12">
-        <input v-model="query" type="text" value="" class="form-control" placeholder="Qidirish...">
+        <input @keyup="searchData()" v-model="query" type="text" value="" class="form-control" placeholder="Qidirish...">
         <button type="submit" class="btn btn-white btn-round btn-just-icon">
           <i class="material-icons">search</i>
           <div class="ripple-container"></div>
         </button>
       </div>
 
-    <div class="dropdown col-sm-12" v-if="query">
+    <div class="dropdown col-sm-12" v-if="query && products.length > 0">
       <ul class="dropdown-menu show col-sm-11" data-style="select-with-transition" >
-        <li v-for="item in products" v-bind:key="item.id">
+        <li v-for="item in filteredProducts" v-bind:key="item.id">
           <a class="dropdown-item"
              href="javascript:void(0)" @click="selectProducts(item.id)">{{ item.name }}</a>
         </li>
@@ -35,34 +29,33 @@
             <div class="card-icon">
               <i class="material-icons">sell</i>
             </div>
-            <h4 class="card-title">Mahsulotlar Table</h4>
+            <h4 class="card-title">Mahsulotlar</h4>
           </div>
           <div class="card-body">
             <div class="table-responsive">
               <table class="table">
                 <thead>
                 <tr>
-                  <th class="text-center">#</th>
-                  <th>Nomi</th>
-                  <th>Real Narxi</th>
-                  <th>Sotilishi</th>
-                  <th class="text-right">Soni</th>
-                  <th class="text-right">Amallar</th>
+                  <th class="text-center">Nomi</th>
+                  <th>Narxi</th>
+                  <th>Soni</th>
+                  <th><i class="material-icons">exposure</i></th>
+                  <th>Amallar</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr v-for="item in selectedProducts" v-bind:key="item.id">
-                  <td class="text-center">{{ item.id }}</td>
                   <td>{{ item.name }}</td>
-                  <td>{{ item.realPrice }}</td>
-                  <td>{{ item.sellPrice }} {{ item.valyuta}}</td>
-                  <td class="text-right">{{ item.soni }}</td>
-
+                  <td>{{ item.price }} {{ item.valyuta }}</td>
+                  <td>{{ item.count }}</td>
+                  <td>
+                    <div class="btn-group btn-group-sm">
+                      <button class="btn btn-round btn-info" @click="minusCount(item.product_id)"> <i class="material-icons">remove</i> </button>
+                      <button class="btn btn-round btn-info" @click="plusCount(item.product_id)"> <i class="material-icons">add</i> </button>
+                    </div>
+                  </td>
                   <td class="td-actions text-right">
-                    <button type="button" rel="tooltip" @click="$router.push('/product/edit/' + item.id)" class="btn btn-success btn-round">
-                      <i class="material-icons">edit</i>
-                    </button>
-                    <button type="button" rel="tooltip" @click="deleteItem(item.id, item.name)" class="btn btn-danger btn-round">
+                    <button type="button" rel="tooltip" @click="delItem(item.product_id)" class="btn btn-danger btn-round">
                       <i class="material-icons">close</i>
                     </button>
                   </td>
@@ -70,6 +63,11 @@
                 </tbody>
               </table>
             </div>
+          </div>
+          <div class="card-footer justify-content-center">
+            <button class="btn btn-info" @click="sell()">Sotish</button>
+            <br>
+            <h5>Umumiy summa {{ summ }}</h5>
           </div>
         </div>
       </div>
@@ -88,18 +86,11 @@ export default {
   data(){
     return{
       query:'',
-      types0:[
-        {
-          id:1,
-          name:'12'
-        },
-        {
-          id:2,
-          name:'12'
-        }
-      ],
       products:[],
-      selectedProducts:[]
+      orgProducts:[],
+      selectedProducts:[],
+      filteredProducts:[],
+      summ:0
     }
   },
   async created() {
@@ -108,12 +99,56 @@ export default {
           this.products = res.data
         }
     )
+    this.orgProducts = this.products
     document.title = "Maxsulotlarni sotish | Market App"
   },
   methods:{
+    async sell() {
+      const res = await axios.post('/products/sell', this.selectedProducts)
+      console.log(res.data)
+    },
+
+    searchData:function (){
+
+      this.query = this.query.toUpperCase()
+      this.filteredProducts = this.products.filter(p => p.name.includes(this.query))
+    },
+
+    delItem(id){
+      const indexObj = this.selectedProducts.findIndex(s => s.product_id === id)
+      this.selectedProducts.splice(indexObj, 1);
+    },
+
+    plusCount(id){
+      const indexObj = this.selectedProducts.findIndex(s => s.product_id === id)
+      const prod = this.orgProducts.filter(p => p.id === this.selectedProducts[indexObj].product_id)
+      if (this.selectedProducts[indexObj].count === prod[0].soni){
+        return;
+      }
+      this.selectedProducts[indexObj].count ++
+    },
+
+    minusCount(id){
+      const indexObj = this.selectedProducts.findIndex(s => s.product_id === id)
+      if (this.selectedProducts[indexObj].count === 1){
+        return;
+      }
+      this.selectedProducts[indexObj].count --
+    },
+
     selectProducts(prod){
       const p = this.products.filter(s => s.id === prod)
-      this.selectedProducts.push(p[0])
+      this.selectedProducts.push(
+          {
+            product_id:p[0].id,
+            name:p[0].name,
+            price:p[0].sellPrice,
+            count:1,
+            valyuta:p[0].valyuta
+          }
+      )
+      this.query = '';
+      this.products = this.products.filter(k => k.id !== prod)
       console.log(this.selectedProducts)
     }
   }
